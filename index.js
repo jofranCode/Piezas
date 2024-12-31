f791123852ad781134d50add208a63a2e6425491
jofranCode <jofranCode@proton.me>
2024-12-31 12:00:05 -0300

    Piezas 1.0 [We created the main structure of the project and its initial configurations.]

---
+++ index.js
@@ -0,0 +1,23 @@
+const {app, BrowserWindow} = require('electron');
+
+let mainWindow;
+
+function createWindow(){
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    mainWindow.loadFile('index.html');
+    mainWindow.on('closed', ()=>{
+        mainWindow = null;
+    });
+}
+
+app.whenReady().then(createWindow);
+app.on('window-all-closed', ()=>{
+    if(process.platform !== 'darwin') app.quit();
+});
